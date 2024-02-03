9f19499f180e296aee8c89e2fb5146443f298345
JORDIMAS-PC\xordi <xordimas@gmail.com>
2024-02-03 18:12:24 +0100

    v2: Now it works correctly with different side widths. It takes the bigger one

--- src/app/directives/rounded-corners.directive.ts
+++ src/app/directives/rounded-corners.directive.ts
@@ -6,70 +6,99 @@ import { Directive, ElementRef, Input, Renderer2 } from '@angular/core';
 })
 export class RoundedCornersDirective {
 
-    private readonly observer: MutationObserver;
 
     constructor(
         private element: ElementRef,
         private renderer: Renderer2,
     ) {
-        // Initialize MutationObserver
-        this.observer = new MutationObserver(() => {
-            this.roundCorners(this.element.nativeElement, null);
-        });
+        this.observer = new MutationObserver(() => { this.roundCorners(this.element.nativeElement, null); });
     }
 
     ngOnInit() {
         this.roundCorners(this.element.nativeElement, null);
-        // Observe changes in the element
+        
         this.observeChanges();
     }
 
+    
+
+    roundCorners(e: HTMLElement, radisPare: Array<number> | null) {
+        let unit; // Suposant que tot estigui en les mateixes unitats //
+        
+        const estilElement = e.computedStyleMap();
+        let radis: Array<any> = [];
+
+        if (radisPare === null) {
+            
+            unit = (<any>estilElement.get("border-top-left-radius")!).unit;
+
+            radis = [
+                (<any>estilElement.get("border-top-left-radius")!).value,
+                (<any>estilElement.get("border-top-right-radius")!).value,
+                (<any>estilElement.get("border-bottom-left-radius")!).value,
+                (<any>estilElement.get("border-bottom-right-radius")!).value,
+            ];
+        } else {
+
+            const estilPare = e.parentElement!.computedStyleMap();
+
+            unit = (<any>estilPare.get("padding-top")!).unit;
+            
+            let sumesMarges = [
+                (<any>estilPare.get("padding-top")!).value    + (<any>estilPare.get("border-top-width")!).value    + (<any>estilElement.get("margin-top")!).value,
+                (<any>estilPare.get("padding-left")!).value   + (<any>estilPare.get("border-left-width")!).value   + (<any>estilElement.get("margin-left")!).value,
+                (<any>estilPare.get("padding-bottom")!).value + (<any>estilPare.get("border-bottom-width")!).value + (<any>estilElement.get("margin-bottom")!).value,
+                (<any>estilPare.get("padding-right")!).value  + (<any>estilPare.get("border-right-width")!).value  + (<any>estilElement.get("margin-right")!).value,
+            ];
+
+            // espais // border-radius //
+            //   0    //     0   1     //
+            // 3   1  //               //
+            //   2    //     3   2     //
+            
+            // top left //
+            radis[0] = radisPare[0] - this.mesGran(sumesMarges[3], sumesMarges[0]);
+
+            // top right //
+            radis[1] = radisPare[0] - this.mesGran(sumesMarges[0], sumesMarges[1]);
+
+            // bottom right //
+            radis[2] = radisPare[0] - this.mesGran(sumesMarges[1], sumesMarges[2]);
+
+            // bottom left //
+            radis[3] = radisPare[0] - this.mesGran(sumesMarges[2], sumesMarges[3]);
+
+            this.renderer.setStyle(e, "border-radius",
+                radis[0] + unit + " " +
+                radis[1] + unit + " " +
+                radis[2] + unit + " " +
+                radis[3] + unit + " "
+            );
+        }
+            
+        
+
+        // Llancem la funcio pels fills //
+        e.childNodes.forEach((e: ChildNode) => {
+            if (!e.nodeValue)
+                this.roundCorners(<HTMLElement>e, radis);
+        });
+    
+
+    }
+
+    // Utils //
+    mesPetit(n1: number, n2: number) { return n1 < n2 ? n1 : n2; }
+    mesGran(n1: number, n2: number) { return n1 > n2 ? n1 : n2; }
+
+
+
+    // Observar canvis //
+    private readonly observer: MutationObserver;
     private observeChanges() {
         const config = { attributes: true, childList: true, subtree: true };
         this.observer.observe(this.element.nativeElement, config);
     }
 
-    roundCorners(e: HTMLElement, radiPare: number | null) {
-        let radiCalculat: number;
-        let unit; // Suposant que tot estigui en les mateixes unitats //
-        
-        const estilElement = e.computedStyleMap();
-
-        if (radiPare === null) {
-            let br: any = estilElement.get("border-top-left-radius")!;
-            radiCalculat = br.value;
-            unit = br.unit;
-        } else {
-            
-            let sumaMarges = 0;
-
-            const estilPare = e.parentElement!.computedStyleMap();
-            
-
-            let pl: any = estilPare.get("padding-left")!;
-            let bl: any = estilPare.get("border-left-width")!;
-            let ml: any = estilElement.get("margin-left")!;
-
-            sumaMarges += pl.value;
-            sumaMarges += bl.value;
-            sumaMarges += ml.value;
-            
-            
-            unit = pl.unit;
-            radiCalculat = radiPare - sumaMarges;
-
-            this.renderer.setStyle(e, "border-radius", radiCalculat + unit);
-        }
-            
-        
-
-        // Llancem la funcio pels fills
-        e.childNodes.forEach((e: ChildNode) => {
-            if (!e.nodeValue)
-                this.roundCorners(<HTMLElement>e, radiCalculat);
-        });
-    
-
-    }
 
 }
